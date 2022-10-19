db5b987f4444aaf76e7498f860723f2baefb5e14
RyusukeTanaka <t.suke.1018@gmail.com>
2022-10-19 14:12:23 +0900

    make user_list and split file.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,19 +1,17 @@
 <!doctype html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ExLaravel</title>
-    @livewireStyles
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8468cbd51d.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1 style="text-align: center;">Hello</h1>
-    <div class="container" style="text-align: center;">
         {{ $slot }}
-    </div>
 </body>
 </html>
 
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar">
+    <ul class="system">
+        <h3>System</h3>
+        <li class="lists">
+            <a href="#">User management</a>
+        </li>
+        <li class="lists">
+            <a href="#">Role management</a>
+        </li>
+        <li class="lists" style="margin-bottom: 50px">
+            <a href="#">Permission management</a>
+        </li>
+
+        <h3>Catalog</h3>
+        <li class="lists">
+            <a href="#">Product management</a>
+        </li>
+        <li class="lists">
+            <a href="#">Category management</a>
+        </li>
+    </ul>
+    {{ $slot }}
+</div>
